2e966805428e271b9cb9ae0b5d5f259e4c6d0b2a
jac7az <jac7az@gmail.com>
2025-09-17 19:07:51 +0000

    9/17

--- synthetic/clean-data.py
+++ synthetic/clean-data.py
@@ -11,22 +11,34 @@ def clean_parquet():
         # Connect to local DuckDB
         con = duckdb.connect(database='synthdata.duckdb', read_only=False)
 
-        # Clear and ipmort
+        # Clear and import
         con.execute(f"""
-            -- SQL goes here
             DROP TABLE IF EXISTS synthdata;
             CREATE TABLE synthdata
                 AS
             SELECT * FROM read_parquet('{input_file}');
         """)
-
-
-
-
-
+        ###Create Age column by calculating difference
+        con.execute(f"""
+            ALTER TABLE synthdata ADD COLUMN age INTEGER;
+            UPDATE synthdata
+            SET age=date_diff('year',birth_date,CURRENT_DATE);
+                    """)
+        print("created age column")
+        ###Remove records w/ Null SCORE
+        con.execute(f"""
+                    DELETE FROM synthdata WHERE score IS NULL;
+                    """)
+        print("remove null")
+         ###Remove duplicates
+        con.execute(f"""
+                    CREATE TABLE synthdata_clean AS SELECT DISTINCT * FROM synthdata;
+                    DROP TABLE synthdata;
+                    ALTER TABLE synthdata_clean RENAME TO synthdata;
+                    """)
+        print("remove duplicates")
     except Exception as e:
         print(f"An error occurred: {e}")
-
 
 if __name__ == "__main__":
     clean_parquet()
